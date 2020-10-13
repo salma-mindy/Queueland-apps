2aef2062cd998bca6caa05a7ec24ac2d77740958
salma-mindy <shellyaka998@gmail.com>
2020-10-13 13:31:12 +0000

    Envoi de la version 1

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:queueland/constants.dart';
+import 'package:queueland/routs.dart';
+import 'package:queueland/screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: kTextColor),
+          bodyText2: TextStyle(color: kTextColor),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
